25e3e869ba0b752b3e1c3b52679b02a5244bed04
Beth Schofield <bethmschofield@gmail.com>
2019-02-11 17:27:46 +0000

    first commit

---
+++ lib/cli_project_demo.rb
@@ -0,0 +1,6 @@
+require "cli_project_demo/version"
+
+module CliProjectDemo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cli_project_demo/version.rb
@@ -0,0 +1,3 @@
+module CliProjectDemo
+  VERSION = "0.1.0"
+end
